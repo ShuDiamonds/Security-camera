b57f22478228add3ff2f01cef31300149e72f817
DiamondsShu <fukuda0729android@gmai.com>
2018-08-27 19:15:46 +0900

    review code

--- TestCode/cameratest.py
+++ TestCode/cameratest.py
@@ -2,11 +2,11 @@
 import cv2
 # カメラのキャプチャを開始 --- (*1)
 cam = cv2.VideoCapture(0)
-"""
-cam.set(cv2.CAP_PROP_FPS, 60)           # カメラFPSを60FPSに設定
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) # カメラ画像の縦幅を720に設定
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # カメラ画像の横幅を1280に設定
-"""
+
+cam.set(cv2.CAP_PROP_FPS, 10)           # カメラFPSを60FPSに設定
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) # カメラ画像の縦幅を720に設定
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320) # カメラ画像の横幅を1280に設定
+
 print(cam.get(cv2.CAP_PROP_FPS))
 print(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 print(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -15,7 +15,7 @@ while True:
     # 画像を取得 --- (*2)
     _, img = cam.read()
     # ウィンドウに画像を表示 --- (*3)
-    cv2.imshow('PUSH ENTER KEY', img)
+    cv2.imshow('PUSH ESC KEY', img)
     # escキーが押されたら終了する
     if cv2.waitKey(1) == 0x1b: break
 # 後始末
