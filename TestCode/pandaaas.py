ab616419634b2a9dbf4ea9165edb88f58bbef69d
DiamondsShu <fukuda0729android@gmai.com>
2018-10-17 18:24:05 +0900

    add pandas data processing

--- TestCode/pandaaas.py
+++ TestCode/pandaaas.py
@@ -2,9 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-df = pd.read_csv('./data.csv', index_col=0, parse_dates=True)
+DATE_FORMAT = "%Y_%m_%d %H:%M:%S"
+my_date_parser = lambda d: pd.datetime.strptime(d, DATE_FORMAT)
+df = pd.read_csv('./data.csv', index_col=0, date_parser=my_date_parser, names=["time","x","y"])
 #print(df)
+df["count"]=1
 
-df2 = df[0].resample('T').sum() 
-df2.head()
+
+df2 = df.resample('60min',how='sum')
+print(df.head())
 
