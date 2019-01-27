61628036e2649a69acc87342fabc44e872f38622
DiamondsShu <fukuda0729android@gmai.com>
2019-01-27 17:36:12 +0900

    update data

--- TestCode/cascadesample.py
+++ TestCode/cascadesample.py
@@ -29,20 +29,30 @@ color=(255, 255, 255)
 fontface = cv2.FONT_HERSHEY_SIMPLEX
 
 
+#カスケードファイルの読み込み
+face_cascade = cv2.CascadeClassifier('./cascade/haarcascade_fullbody.xml')
 
 while True:
 	# 画像を取得 --- (*2)
 	_, img = cam.read()
 	_, img2 = cam2.read()
 	img = ndimage.rotate(img, 180, reshape=False)
-
+	gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+	
 	cv2.putText(img,datetime.now().strftime("%Y/%m/%d %H:%M:%S"),(width-120,height-5),fontface,fontscale, color)
 					
 	cv2.putText(img2,datetime.now().strftime("%Y/%m/%d %H:%M:%S"),(width-120,height-5),fontface,fontscale, color)
-
+	
+	#カスケードファイルと使って顔認証
+	faces = face_cascade.detectMultiScale(gray)
+	for (x,y,w,h) in faces:
+		#部分を四角で囲う
+		cv2.rectangle(img2,(x,y),(x+w,y+h),(255,0,0),2)
+	cv2.imshow('PUSH ESC KEY',img2)
+	
 	# ウィンドウに画像を表示 --- (*3)
-	cv2.imshow('PUSH ESC KEY', img)
-	cv2.imshow('PUSH ESC KEY2', img2)
+	#cv2.imshow('PUSH ESC KEY', img)
+	#cv2.imshow('PUSH ESC KEY2', img2)
 	# escキーが押されたら終了する
 	if cv2.waitKey(1) == 0x1b: break
 # 後始末
